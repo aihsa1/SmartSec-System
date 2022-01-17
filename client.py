b39192dc2690d8f8def2f2931261006b3dba9b48
Ran <randavid17@gmail.com>
2022-01-17 18:42:07 +0200

    added a basic found/not found indicator

--- client.py
+++ client.py
@@ -14,6 +14,9 @@ paths = None
 files = None
 labels = None
 
+MIN_SCORE_THRESH = 0.5
+MAX_BOXES_TO_DRAW = 5
+
 
 def communication():
     pass
@@ -74,15 +77,15 @@ def detection_interface(frame):
         detections['detection_scores'],
         category_index,
         use_normalized_coordinates=True,
-        max_boxes_to_draw=5,
-        min_score_thresh=0.5,
+        max_boxes_to_draw=MAX_BOXES_TO_DRAW,
+        min_score_thresh=MIN_SCORE_THRESH,
         agnostic_mode=False)
-    return frame_np_with_detections
+    return detections, frame_np_with_detections
 
 ####################GUI####################
 
 
-def gui():
+def gui(detection_mode=True):
     # GUI INITIALIZATION
     cap = cv2.VideoCapture(0)
     WIDTH_WEBCAM = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -90,12 +93,14 @@ def gui():
     cap.release()
 
     w, h = int(WIDTH_WEBCAM * 1.3), int(HEIGHT_WEBCAM * 1.3)
-
+    
     layout = [
-        [sg.Text("Webcam Feed With Detections", justification="center")],
+        [sg.Text("Webcam Feed With Detections", justification="center", font=(*sg.DEFAULT_FONT, "bold underline"), key="-TITLE-")],
+        [sg.Text("Weapon Not Found", justification="left", key="-FOUND-INDICATOR-")],
         [sg.Image(filename='', key="-VIDEO-", enable_events=True, size=(WIDTH_WEBCAM, HEIGHT_WEBCAM), pad=(((w - WIDTH_WEBCAM)
-                  * 0.5, (h - HEIGHT_WEBCAM) * 0.5, (0, 0))))],
-        [sg.VSeparator()]
+                  * 0.5, (h - HEIGHT_WEBCAM) * 0.5, (0, 0))), background_color="black")],
+        [sg.VSeparator()],
+
     ]
 
     cap = cv2.VideoCapture(0)
@@ -107,8 +112,16 @@ def gui():
         event, value = window.read(timeout=5)
         if event == sg.WIN_CLOSED:
             break
+
+        if detection_mode:
+            detections, frame = detection_interface(frame)
+            if window["-FOUND-INDICATOR-"].get() == "Weapon Not Found" and len(detections["detection_scores"][detections["detection_scores"] >=  MIN_SCORE_THRESH]) > 0:
+                window["-FOUND-INDICATOR-"].update("Weapon Found")
+                pass
+            else:
+                window["-FOUND-INDICATOR-"].update("Weapon Not Found")
+                pass
         
-        frame = detection_interface(frame)
         frame_bytes = cv2.imencode(".png", frame)[1].tobytes()
         window["-VIDEO-"].update(data=frame_bytes)
 
@@ -119,8 +132,10 @@ def gui():
 
 
 def main():
-    initialize_model()
-    gui()
+    detection_mode = True
+    if detection_mode:
+        initialize_model()
+    gui(detection_mode)
 
 
 if __name__ == "__main__":
