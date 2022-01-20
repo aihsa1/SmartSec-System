3cf51d3b65ee6d3ef4a5ce6b9773a7c9c8ae8475
Ran <randavid17@gmail.com>
2022-01-20 16:35:40 +0200

    completed Message class with bytes dtype support

--- Classes/Message.py
+++ Classes/Message.py
@@ -3,20 +3,28 @@ class Message:
         """
         Ths function is used to initialize the message object.
         :param message: The message to be sent
-        :type message: str
+        :type message: str/bytesr
         :param header_size: The size of the header of the communication protocol
         :type header_size: int
         :param message_size: The size of the message to be sent. If not specified, the message size is assumed to be the size of the message. otherwise, the message size is assumed to be the specified size. It should be noted that specifiying the message size is optional, and it is used only to create a message and acummulate buffered data to it.
         :type message_size: int/None
         """
+        # set is_compelete and message_size according to the params
         if message_size is not None:
             self.message_size = message_size
             self.is_complete = False
         else:
             self.message_size = len(message)
             self.is_complete = True
+        
         self.header_size = header_size
-        self.message = f"{self.message_size: <{self.header_size}}" + message
+
+        if isinstance(message, bytes):
+            self.message = f"{self.message_size: <{self.header_size}}".encode() + message
+        else:
+            self.message = f"{self.message_size: <{self.header_size}}" + message
+
+        
 
     def get_plain_msg(self):
         """
@@ -24,7 +32,7 @@ class Message:
         :return: The plain message
         :rtype: str
         """
-        return self.message[self.header_size:]
+        return self.message[self.header_size:]# takes str and bytes into account
     
     @classmethod
     def create_message_from_plain_data(cls, plain_data, header_size):
@@ -34,6 +42,8 @@ class Message:
         :type plain_data: str
         :param header_size: The size of the header of the communication protocol
         :type header_size: int
+        :return: The message object
+        :rtype: Message
         """
         msg = plain_data[header_size:]
         if int(plain_data[:plain_data.find(" ")]) == len(msg):
@@ -44,10 +54,17 @@ class Message:
         """
         This function is used to accumulate the message ON RECIEVEING ONLY
         :param string: The string to be added to the message
-        :type string: str
+        :type string: str/bytes
+        :return: The message object with the added data
+        :rtype: Message
         """
         if self.is_complete:
             raise ValueError("The message is already complete")
+        if not((isinstance(string, str) and isinstance(self.message, str)) or (isinstance(string, bytes) and isinstance(self.message, bytes))):
+            raise TypeError("The message and the string must be of the same type")
+        if len(string) + len(self.message[self.header_size:]) > self.message_size:
+            raise ValueError(f"the given string is too long. It should be at most {self.message_size - len(self.message[self.header_size:])}, and you gave {len(string)}")
+        
         self.message += string
         self.is_complete = len(self.get_plain_msg()) == self.message_size
         return self
@@ -56,19 +73,19 @@ class Message:
         """
         This function is used to return the header + message
         :return: The header + message
-        :rtype: str
+        :rtype: str/bytes
         """
-        return self.message
+        return self.message.__str__()# takes str and bytes into account
 
 
 def main():
-    m = Message("Hello", 20, 11)
-    print(m)
-    print(m.is_complete)
-    m += "World!"
-    print(m)
-    print(m.is_complete)
-    m += "!"
+    # m = Message("Hello", 20, 11)
+    # print(m)
+    # print(m.is_complete)
+    # m += "World!"
+    # print(m)
+    # print(m.is_complete)
+    # m += "!"
 
     # m = Message.create_message_from_plain_data("15      Hello World!", 8)
     # print(m)
@@ -77,6 +94,18 @@ def main():
     # print(m)
     # print(m.is_complete)
     # m += "456"
+
+    # m = Message("123".encode(), 8, 6)
+    # print(m.get_plain_msg())
+    # m += "456".encode()
+    # print(m.get_plain_msg())
+    # print(m.message_size)
+    # print(m)
+
+    m = Message("123".encode(), 8, 6)
+    print(m.message_size)
+    m += "4567".encode()
+    print(m)
 
 if __name__ == "__main__":
     main()
