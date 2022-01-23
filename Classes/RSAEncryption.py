7e8f47b1cefe2ddf8ba5f38214030244b12babb0
Ran <randavid17@gmail.com>
2022-01-23 19:25:21 +0200

    fixed the wrong RSA Class implementation

--- Classes/RSAEncryption.py
+++ Classes/RSAEncryption.py
@@ -3,27 +3,41 @@ import rsa
 class RSAEncyption:
     HASH_METHOD = "SHA-256"
     def __init__(self) -> None:
-        self.my_keys = None
-        self.other_privkey = None
+        self.my_pubkey = None
+        self.my_privkey = None
+        self.other_pubkey = None
     
     def generate_keys(self):
-        self.my_keys = rsa.newkeys(1024)
+        self.my_pubkey, self.my_privkey = rsa.newkeys(1024)
     
     def encrypt(self, msg):
-        return rsa.encrypt(msg.encode(), self.my_keys[0])
+        """
+        Encrypt a message using the senders public key
+        :param msg: The message to decrypt
+        :type msg: str
+        :return: The decrypted message
+        :rtype: bytes
+        """
+        return rsa.encrypt(msg.encode(), self.other_pubkey)
     
-    def decrypt(self,ciphertext):
+    def decrypt(self, ciphertext):
+        """
+        Decrypt the incoming message using our private key
+        :param ciphertext: The encrypted message
+        :type ciphertext: bytes
+        :return: The decrypted message
+        """
         try:
-            return rsa.decrypt(ciphertext, self.other_privkey).decode()
+            return rsa.decrypt(ciphertext, self.my_privkey).decode()
         except:
             return False
     
     def generate_signature(self, msg):
-        return rsa.sign(msg.encode(), self.my_keys[1], RSAEncyption.HASH_METHOD)
+        return rsa.sign(msg.encode(), self.my_privkey, RSAEncyption.HASH_METHOD)
     
     def verify_signature(self, msg, signature):
         try:
-            return rsa.verify(msg.encode(), signature, self.other_privkey) == RSAEncyption.HASH_METHOD
+            return rsa.verify(msg.encode(), signature, self.other_pubkey) == RSAEncyption.HASH_METHOD
         except Exception as e:
             print(e)
             return False
@@ -32,8 +46,9 @@ def main():
     client, server = RSAEncyption(), RSAEncyption()
     client.generate_keys(); server.generate_keys()
     ##########start private key key exchange############
-    client.other_privkey = server.my_keys[1]; server.other_privkey = client.my_keys[1]
-    ##############start key exchange############
+    # every side gets the other side's public key so he can encrypt
+    client.other_pubkey = server.my_pubkey; server.other_pubkey = client.my_pubkey
+    ##############end key exchange############
     msg = input("=> ")
     ciphertext = client.encrypt(msg)
     signature = client.generate_signature(msg)
