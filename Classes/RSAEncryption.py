94abc4226404ace1d8f5698335d2ff8ce9433925
Ran <randavid17@gmail.com>
2022-01-23 19:37:38 +0200

    added documentation for RSA class

--- Classes/RSAEncryption.py
+++ Classes/RSAEncryption.py
@@ -3,16 +3,22 @@ import rsa
 class RSAEncyption:
     HASH_METHOD = "SHA-256"
     def __init__(self) -> None:
+        """
+        The constructor for the RSAEncyption class. Every instance of this class is used as an interface to the RSA algorithm.
+        """
         self.my_pubkey = None
         self.my_privkey = None
         self.other_pubkey = None
     
     def generate_keys(self):
+        """
+        This method generates a public and private key pair.
+        """
         self.my_pubkey, self.my_privkey = rsa.newkeys(1024)
     
     def encrypt(self, msg):
         """
-        Encrypt a message using the senders public key
+        Encrypts a message using the senders public key
         :param msg: The message to decrypt
         :type msg: str
         :return: The decrypted message
@@ -22,10 +28,10 @@ class RSAEncyption:
     
     def decrypt(self, ciphertext):
         """
-        Decrypt the incoming message using our private key
+        Decrypt the incoming ciphertext using our private key. This ciphertext is encrypted by the sender using our public key.
         :param ciphertext: The encrypted message
         :type ciphertext: bytes
-        :return: The decrypted message
+        :return: The decrypted message. If something went wrong, False is returned
         """
         try:
             return rsa.decrypt(ciphertext, self.my_privkey).decode()
@@ -33,9 +39,17 @@ class RSAEncyption:
             return False
     
     def generate_signature(self, msg):
+        """
+        This method generates a signature for the given message. The signature is generated using our private key, so the receiver will be able to authenticate the  received message.
+        :param msg: The message to sign
+        :type msg: str
+        """
         return rsa.sign(msg.encode(), self.my_privkey, RSAEncyption.HASH_METHOD)
     
     def verify_signature(self, msg, signature):
+        """
+        This method verifies the signature of the given message. The signature is generated using the sender's public key, so we will be able to authenticate the incoming message.
+        """
         try:
             return rsa.verify(msg.encode(), signature, self.other_pubkey) == RSAEncyption.HASH_METHOD
         except Exception as e:
@@ -45,7 +59,7 @@ class RSAEncyption:
 def main():
     client, server = RSAEncyption(), RSAEncyption()
     client.generate_keys(); server.generate_keys()
-    ##########start private key key exchange############
+    ##########start private key key exchange in the public domain############
     # every side gets the other side's public key so he can encrypt
     client.other_pubkey = server.my_pubkey; server.other_pubkey = client.my_pubkey
     ##############end key exchange############
