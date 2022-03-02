e88d7260ed24614c3d4978769e03b36c0b524c25
Ran <randavid17@gmail.com>
2022-03-02 09:28:24 +0200

    made basic unfinished multiplexed video GUI

--- Screens/detection_gui.py
+++ Screens/detection_gui.py
@@ -4,7 +4,8 @@ import PySimpleGUI as sg
 
 def generate_detection_gui_layout():
 
-    # N_CAMERAS = 2
+    N_CAMERAS = 4
+    N_IN_ROWS = 2
 
     cap = cv2.VideoCapture(0)
     WIDTH_WEBCAM = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -13,25 +14,29 @@ def generate_detection_gui_layout():
 
     w, h = int(WIDTH_WEBCAM * 1.3), int(HEIGHT_WEBCAM * 1.3)
 
+    cameras_layout = []
+    for i in range(N_CAMERAS//2):
+        row = []
+        for j in range(2):
+            row.append(sg.Image(filename='', key=f"-VIDEO{i * N_IN_ROWS + j}-", enable_events=True, size=(WIDTH_WEBCAM // N_IN_ROWS, HEIGHT_WEBCAM // N_IN_ROWS), background_color="black", tooltip=f"Cam {i * N_IN_ROWS + j}"))
+        cameras_layout.append(row)
+    if N_CAMERAS % N_IN_ROWS == 1:
+        cameras_layout.append(sg.Image(filename='', key=f"-VIDEO{N_CAMERAS}-", enable_events=True, size=(WIDTH_WEBCAM // N_IN_ROWS, HEIGHT_WEBCAM // N_IN_ROWS), background_color="black", tooltip=f"Cam {N_CAMERAS}"))
+
+
     layout = [
         [sg.Text("Webcam Feed With Detections", justification="center",
                  font=(*sg.DEFAULT_FONT, "bold underline"), key="-TITLE-")],
         [sg.Text("Weapon Not Found", justification="left",
                  key="-FOUND-INDICATOR-")],
         [
-            sg.Column(
-                [
-                    [
+            [
                         sg.Button(button_text="<", key="-BUTTON-NEXT-",
                                   enable_events=True, size=(int(WIDTH_WEBCAM * 0.005), int(HEIGHT_WEBCAM * 0.005))),
                         sg.Button(button_text=">", key="-BUTTON-PREV-",
                                   enable_events=True, size=(int(WIDTH_WEBCAM * 0.005), int(HEIGHT_WEBCAM * 0.005)))
-                    ],
-                    [
-                        sg.Image(filename='', key="-VIDEO-", enable_events=True, size=(WIDTH_WEBCAM, HEIGHT_WEBCAM), pad=(
-                            ((w - WIDTH_WEBCAM) * 0.5, (h - HEIGHT_WEBCAM) * 0.5, (0, 0))), background_color="black", tooltip=f"Cam 1"),
-                    ]
-                ], element_justification="center")
+            ],
+            cameras_layout
         ],
         [sg.VSeparator()],
     ]
@@ -46,9 +51,5 @@ if __name__ == "__main__":
         event, value = window.read(timeout=10)
         if event == sg.WIN_CLOSED:
             break
-        # if event == "-BUTTON-NEXT-":
-        #     i += 1
-        # if event == "-BUTTON-PREV-":
-        #     i -= 1
 
     window.close()
