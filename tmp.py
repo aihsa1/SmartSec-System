3b7b4c2c14479468c0cad5a7692886f36d7e59bc
Ran <randavid17@gmail.com>
2022-03-25 18:55:42 +0300

    experimenting with mongodb

--- tmp.py
+++ tmp.py
@@ -1,23 +1,8 @@
-from Classes.Message import Message
-import numpy as np
-# DeprecationWarning: `np.str` is a deprecated alias for the builtin `str`. To silence this warning, use `str` by itself. Doing this will not modify 
-# any behavior and is safe. If you specifically wanted the numpy scalar type, use `np.str_` here.
-# Deprecated in NumPy 1.20; for more details and guidance: https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations
-#   ("name", np.str),
-# c:\Users\USER\Desktop\Cyber\PRJ\tmp.py:12: DeprecationWarning: `np.int` is a deprecated alias for the builtin `int`. To silence this warning, use `int` by itself. Doing this will not modify 
-# any behavior and is safe. When replacing `np.int`, you may wish to use e.g. `np.int64` or `np.int32` to specify the precision. If you wish to review your current use, check the release note 
-# link for additional information.
-# Deprecated in NumPy 1.20; for more details and guidance: https://numpy.org/devdocs/release/1.20.0-notes.html#deprecations
-#   ("age", np.int)
+import pymongo
 
-arr = np.array(
-    [
-        ("ran", 17),
-        ("david", 18)
-    ], 
-    dtype=[
-        ("name", (str, 11)),
-        ("age", int)
-    ]
-)
-print(arr["name"])
+USERNAME = "KyHSAVsQnqTw1HC0"
+PASSWORD = "XnHZPdap8941pypr"
+
+
+mongodb_client = pymongo.MongoClient(F"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.pueke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+print(mongodb_client.list_database_names())
